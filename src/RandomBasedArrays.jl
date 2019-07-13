53650b59e17e4b210a16f4d3d9b30c874681d7f0
Mosè Giordano <m.giordano@ucl.ac.uk>
2019-07-13 02:16:04 +0100

    Start the package

--- src/RandomBasedArrays.jl
+++ src/RandomBasedArrays.jl
@@ -1,5 +1,21 @@
 module RandomBasedArrays
 
-greet() = print("Hello World!")
+export RandomBasedArray
+
+struct RandomBasedArray{T,N} <: AbstractArray{T,N}
+    parent::Array{T,N}
+end
+
+index(p::Array) = firstindex(p) + rand(eachindex(p)) % lastindex(p)
+
+Base.size(A::RandomBasedArray) = size(A.parent)
+Base.getindex(A::RandomBasedArray, ::Int) =
+    @inbounds getindex(A.parent, index(A.parent))
+Base.getindex(A::RandomBasedArray{T,N}, ::Vararg{Int,N}) where {T,N} =
+    getindex(A.parent, Base._ind2sub(A.parent, index(A.parent))...)
+Base.setindex!(A::RandomBasedArray, v, ::Int) =
+    @inbounds setindex!(A.parent, v, index(A.parent))
+Base.setindex!(A::RandomBasedArray{T,N}, v, ::Vararg{Int,N}) where {T,N} =
+    setindex!(A.parent, v, Base._ind2sub(A.parent, index(A.parent))...)
 
 end # module
