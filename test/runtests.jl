d9511273e230c50b0ccebc71db69dbedd20df7dd
Mosè Giordano <m.giordano@ucl.ac.uk>
2019-07-16 11:08:41 +0100

    Add some tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,19 @@
 using RandomBasedArrays
-using Test
+using Random, Test
 
 @testset "RandomBasedArrays.jl" begin
-    # Write your own tests here.
+    Random.seed!(1)
+    p = reshape(collect(1:6), 2, 3)
+    A = RandomBasedArray(p)
+    @test parent(A) === p
+    @test parent(A) == [1 3 5; 2 4 6]
+    @test size(A) === size(p)
+    @test A[42] == 1
+    @test A[-35] == 1
+    @test A[4,0] == 6
+    A[-314] = 0
+    @test parent(A) == [1 3 5; 2 0 6]
+    A[0,0] = 42
+    @test parent(A) == [1 3 5; 42 0 6]
+    @test repr(A) == "[3 1 5; 5 5 1]"
 end
